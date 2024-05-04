e566a3725d5ea3fec7221d5c4494707e09dece08
JScoder4005 <muk4005@gmial.com>
2024-05-04 14:25:52 +0530

    complete signup page functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 
 const SignUp = () => {
   const [formData, setFormData]=useState({});
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleChange =(e) => {
     setFormData(
       {
         ...formData,
-        [e.target.value]: e.target.value
+        [e.target.id]: e.target.value
       }
     )
   };
-  console.log(formData)
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+ // console.log(formData)
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
     const res = await fetch('/api/auth/signup', {
       method: 'POST',
       headers: {
@@ -25,9 +30,22 @@ const SignUp = () => {
       },
       body: JSON.stringify(formData),
     });
-    
-    const data = await res.json();
-    console.log(data, 'USERS');
+    try {
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate('/sign-in');
+      console.log(data)
+    } catch (err) { 
+      setLoading(false);
+      setError(err.message);
+      console.log(err)
+    }
   }
 
   
@@ -39,14 +57,15 @@ const SignUp = () => {
         <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' onChange={handleChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg ' id='email'onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg ' id='password' onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:bg-opacity-80'>Sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:bg-opacity-80'>{loading ? 'Loading..' : 'Sign Up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
         <Link to={'/sign-in'}>
-          <span className='text-blue-700'>Sign in</span>
+          <span className='text-blue-800'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-600 mt-5'>{error}</p>}
     </div>
   )
 }
