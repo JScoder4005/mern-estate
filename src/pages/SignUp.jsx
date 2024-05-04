00fe9c16c625629f787af86e54b7e7243f92cc8b
JScoder4005 <muk4005@gmial.com>
2024-05-04 13:17:42 +0530

    signup UI changes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+
 const SignUp = () => {
+  const [formData, setFormData]=useState({});
+  const [error, setError] = useState(null);
+
+  const handleChange =(e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.value]: e.target.value
+      }
+    )
+  };
+  console.log(formData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    
+    const data = await res.json();
+    console.log(data, 'USERS');
+  }
+
+  
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 '>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg ' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg ' id='password' />
+      <form className='flex flex-col gap-4 ' onSubmit={handleSubmit}>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' onChange={handleChange}/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg ' id='email'onChange={handleChange} />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg ' id='password' onChange={handleChange}/>
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:bg-opacity-80'>Sign up</button>
       </form>
       <div className='flex gap-2 mt-5'>
