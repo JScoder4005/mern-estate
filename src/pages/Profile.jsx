fce6006260a544c579b2ae4643a2fcba1055013a
JScoder4005 <muk4005@gmial.com>
2024-06-30 22:41:50 +0530

    Complete Update Listing Functionality...

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -349,9 +349,11 @@ const Profile = () => {
                                 >
                                     Delete
                                 </button>
-                                <button className="text-green-700 uppercase">
-                                    Edit
-                                </button>
+                                <Link to={`/update-listing/${listing._id}`}>
+                                    <button className="text-green-700 uppercase">
+                                        Edit
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     ))}
